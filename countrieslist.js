911dbc3862de14fa5f1b20008db64ec63f5223a6
panchenelo <45730607+panchenelo@users.noreply.github.com>
2020-06-21 21:18:01 +0300

    Create countrieslist.js

---
+++ countrieslist.js
@@ -0,0 +1,54 @@
+(function() {
+    'use strict';
+    window.countryList = {
+        ru: {
+            name: 'Россия',
+            code: 'ru',
+            price: 990,
+            oldPrice: 1980,
+            labelPrice: ' руб.',
+            phoneHelper: 'Например +7 9031234567',
+            nameHelper: 'Екатерина Самойлова',
+            nameError: 'Имя обязательно для заполнения',
+            phoneError: 'Телефон обязателен для заполнения',
+            countryError: 'Страна обязательна для заполнения',
+            isDefault: true
+        },
+        ua: {
+            name: 'Украина',
+            code: 'ua',
+            price: 399,
+            oldPrice: 798,
+            labelPrice: ' грн.',
+            phoneHelper: 'Например +380 123456789',
+            nameHelper: 'Анастасія Кравчук',
+            nameError: 'Імя обовязково для заповнення',
+            phoneError: 'Телефон обовязковий для заповнення',
+            countryError: 'Країна обовязковий для заповнення'
+        },
+         by: {
+            name: 'Беларусь',
+            code: 'by',
+            price: 35,
+            oldPrice: 70,
+            labelPrice: ' руб',
+            phoneHelper: 'Например +375 123456789',
+            nameHelper: 'Маргарыта Концява',
+            nameError: 'Імя абавязковы для запаўнення',
+            phoneError: 'Тэлефон абавязковы для запаўнення',
+            countryError: 'Краіна абавязковы для запаўнення'
+        },
+         kz: {
+            name: 'Казахстан',
+            code: 'kz',
+            price: 5490,
+            oldPrice: 10980,
+            labelPrice: ' тг.',
+            phoneHelper: 'Например +77 123456789',
+            nameHelper: 'Айжаксым Омарова',
+            nameError: 'Имя обязательно для заполнения',
+            phoneError: 'Телефон обязателен для заполнения',
+            countryError: 'Страна обязательна для заполнения'
+        }
+    };
+})();
